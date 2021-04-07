e98f1eaca13e7b8783c789183f1e8a3913e7aa33
Lucas Veltri <veltri.lucas@live.fr>
2021-04-07 08:55:28 +0200

    Changing elements
    Change domain shape,
    adding some element

--- 3dInterface.js
+++ 3dInterface.js
@@ -1,36 +1,37 @@
-//interface 3d
+/*==========================================================
+	Interface 3d Mosca --> P5Js
+	Lucas Veltri @2021 --> SCRIME
+==========================================================*/
 
-
-let nbSources = 10;
-
-let cube;
+let nbSources = 5;
+let main;
 let sources=[];
 let r =10, g = 10, b = 10;
-
 let reset;
+let orbitButton; 
 
+let sphereDomain;
+let cnv;
 function setup(){
-	createCanvas(800,800,WEBGL);
+
+	cnv = createCanvas(800,800,WEBGL);
 	angleMode(DEGREES);
-
-	cube = new domain(0,0,400);	
+/*	cube = new domain(0,0,0,400)*/	
 	for(let i = 0; i<nbSources; i++){
-		sources[i] = new speaker(random(-150,150),random(-150,150),random(-150,150),r,g,b,i);
+			sources[i] = new speaker(random(-150,150),random(-150,150),random(-150,150),r,g,b,i);
+
 	}
-
 	menu();
-
-	//debugMode();
+	main = new userMain(0,0,0,10);
+	sphereDomain = new domain(0,0,0,200);
 
 }
-function draw(){
 
+function draw(){
 	background(220);
-	//print("source 1 X :" + sources[0].x);
-	cube.draw();
-	translate(0,0,cube.size/2);//redef du centre en (0,0,0);
-	
-	
+	main.draw(100,0,0);
+	sphereDomain.draw();
+
 	for(i = 0; i<nbSources; i++){ //creation des sources
 		push();
 			sources[i].colorS(r,g,b);
@@ -44,6 +45,9 @@ function draw(){
 			sources[i].R = 0;
 			sources[i].G = 190;
 			sources[i].B = 255;
+
+
+	
 		}
 		else{
 			sources[i].R = 0;
@@ -51,72 +55,73 @@ function draw(){
 			sources[i].B = 0;
 		}
 	}
-	if(selector.selected() == 0){ //mouvement de la "camera"
-		orbitControl();
+
+	/*change la couleur de la source indiquant que la source se situe en dehors du domaine.*/	
+	for(i = 0; i < nbSources; i++){
+		let dSources2Cube = dist(main.x,main.y,main.z,sources[i].x,sources[i].y,sources[i].z);
+		if(dSources2Cube> 200 || dSources2Cube< -200){
+			sources[i].R = 255;
+		}
 	}
 
+
+	//Selection de la source avec souris le plus proche possible
 	for(i= 0; i < nbSources; i++ ){
 		if(mouseIsPressed && selector.selected() != 0 && range(mouseX,mouseY)){
-			sources[selector.selected()-1].z = -(mouseY-height/2);
+			sources[selector.selected()-1].y = (mouseY-height/2);
 			if(keyIsDown(89)){
-				sources[selector.selected()-1].y = mouseX-width/2;
+				sources[selector.selected()-1].z = -(mouseX-width/2);
 			}
 			else{
 				sources[selector.selected()-1].x = mouseX-width/2;
-			}
-			
-			
+			}	
 		}
 	}
+
+	//controlOrbit Activation
+	if(activeO ==1){
+		orbitControl(1,0,0);
+	}
+	else{
+		orbitControl(0,0,0);
+	}
+
+
 
 
 }
 
 
-//aditional function
+/*Revoir la position de la souris
+en placant les sources proportionnellement
+a la taile du domaine*/
+
 function mouseWheel(event){
-	cube.size += event.delta;
-	//pevoire d'empecher l 'inversement de la structure
+	let sizer = sphereDomain.size + event.delta;
+	if(sizer > 0 && sizer < 400){
+		sphereDomain.size += event.delta;
+		/*for(i = 0; i<nbSources; i++){
 
 
-	//creer un refresh des positions des sources
+			if(sources[i].x > 0){
+				sources[i].x += event.delta/2;
+			}
+			else{
+				sources[i].x -= event.delta/2;
+			}
+		}	*/
+
+		/*utiliser dist() pur recuperer la distance a l origine pour chaque et rajouter le coef
+		de distance capturé par la souris pour placer proportionnellement les nouvelle places des sources*/
+
+	}
+
 }
 
 //marge pour la position de la souris
 function range(X,Y){
 	if(X < 750 && X > 50 && Y < 750 && Y > 50){
 		return true;
-	}
-function keyTyped(){}
-
-}
-
-
-
-
-//classes
-class domain{
-	constructor(_x,_y,_size){
-		this.x = _x;
-		this.y = _y;
-		this.size = _size;
-	}
-
-	draw(){
-		stroke(0);
-		noFill();
-		//rotateY(millis() /-90);
-		translate(0,0);
-		box(this.size);
-		noStroke();
-		fill(100);
-		rotateX(90);
-		translate(0,0,-this.size/2);
-		plane(this.size);
-
-		translate(0,0,0);
-		
-
 	}
 }
 
@@ -128,22 +133,54 @@ class speaker{
 		this.R = _R;
 		this.G = _G;
 		this.B = _B;
-		this.number = _sourceNumber;
+		//this.number = _sourceNumber;
 
 	}
-	draw(){
+	draw(_sizeSource = 10){
+		this.sizeSource = _sizeSource;
 		noStroke();
 		translate(this.x,this.y,this.z);
 		sphere(10);
+
 	}
-	colorS(R,G,B){
+	colorS(){
 		fill(this.R,this.G,this.B);
-	}
-	move(){
-		
 	}
 	ranged(){
 		let rDist = dist(mouseX-width/2,mouseY-height/2,this.x,this.y);
 		if(dist < 5){return true};
+	}
+}
+class userMain{
+	constructor(_x,_y,_z,size){
+		this.x = _x;
+		this.y = _y;
+		this.z = _z;
+		this.size = size;
+	}
+	draw(r,g,b){
+		this.r =r;
+		this.g =g;
+		this.b =b;
+
+		translate(this.x,this.y,this.z);
+		fill(this.r,this.g,this.b);
+		stroke(100,0,0);
+
+		sphere(this.size);
+	}
+}
+class domain{ //domaine son
+	constructor(x,y,z,size){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.size=size;
+	}
+	draw(){
+		translate(this.x,this.y,this.z);
+		noFill();
+		stroke(0);
+		sphere(this.size);
 	}
 }
--- 3dInterfaceMenu.js
+++ 3dInterfaceMenu.js
@@ -1,31 +1,65 @@
 let selector;
 let sourcesNumber;
-let a;
+let a = 0;
+let activeO;
+let p;
+let p2;
+
+
 
 function menu(){
 	//menu
 	selector = createSelect();
-	//textFont(ari);
-	//text('sources');
+	selector.position(10,40);
 	sourcesNumber = createSelect();
 	sourcesNumber.position(10,10);
 
 
-	for(let i = 0; i<=18; i++){
+	//Creation des options de debugage et orbitControl
+	orbitButton = createCheckbox('Orbit Control',false);
+	orbitButton.position(690,10);
+	orbitButton.changed(activeOrbit);
+
+	debugCbx = createCheckbox('Debug Mode',false);
+	debugCbx.position(690,30);
+	debugCbx.changed(activeDebug);
+
+
+	p2 = createP('quantité de sources');
+	p = createP('selection des sources');
+	p2.position(55,-7);
+	p.position(55,23);
+
+
+
+	for(let i = 0; i<=64; i++){
 		sourcesNumber.option(''+i,i);
 		
 	}
 	sourcesNumber.selected(0);
 	
 	a = sourcesNumber.value();
-
-	for(let j = 0; j< nbSources +1; j++){
+		for(let j = 0; j< nbSources + 1; j++){
 		selector.option(""+j,j);
 	}
 	selector.selected(0);
 
-	
-	//prevoir les textes
-	
+
+}
+function activeOrbit(){
+
+	if(orbitButton.checked()){
+		activeO = 1;
+	}
+	else{
+		activeO =0;
+	}
 }
 
+function activeDebug(){
+	if(this.checked()){
+		debugMode();
+			}else{
+		noDebugMode();
+	}
+}
